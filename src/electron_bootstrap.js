4179a4723d7fbe7b5c07309b2ca2130d76f02eda
nilrem <nilremdev@gmail.com>
2026-06-28 12:46:36 +0900

    add electron project to built angular webapp output

---
+++ src/electron_bootstrap.js
@@ -0,0 +1,16 @@
+console.log("Hello from Electron!")
+
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 1000
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+});
