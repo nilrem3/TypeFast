4d2624038bb22566d70808547332e33662a6c8cc
nilrem <nilremdev@gmail.com>
2026-07-24 17:46:40 +0900

    add headers to columns when creating new spreadsheet

--- src/electron_bootstrap.js
+++ src/electron_bootstrap.js
@@ -30,6 +30,61 @@ function handleCreateSheet(event, props) {
     }
     let path = results.filePath;
     XlsxPopulate.fromBlankAsync().then(workbook => {
+      const sheet = workbook.addSheet("Data");
+      sheet.cell("A1").value("Native English Speaker?")
+      .relativeCell(0, 1).value("Keyboard Layout")
+      .relativeCell(0, 1).value("Taken a Typing Class?")
+      .relativeCell(0, 1).value("Has Musical Training?")
+      .relativeCell(0, 1).value("Musical Training Duration")
+
+      .relativeCell(0, 1).value("Test Order")
+
+      .relativeCell(0, 1).value("No Music Correct Characters")
+      .relativeCell(0, 1).value("No Music Incorrect Characters")
+      .relativeCell(0, 1).value("No Music Correct Words")
+      .relativeCell(0, 1).value("No Music Incorrect Words")
+      .relativeCell(0, 1).value("No Music Space Characters")
+      .relativeCell(0, 1).value("No Music Space Characters (Unnecessary)")
+      .relativeCell(0, 1).value("No Music Time (Seconds)")
+      .relativeCell(0, 1).value("No Music Character Accuracy")
+      .relativeCell(0, 1).value("No Music Characters per Minute")
+      .relativeCell(0, 1).value("No Music Word Accuracy")
+      .relativeCell(0, 1).value("No Music Words per Minute")
+      .relativeCell(0, 1).value("No Music Real Words per Minute")
+
+      .relativeCell(0, 1).value("Familiar Music Correct Characters")
+      .relativeCell(0, 1).value("Familiar Music Incorrect Characters")
+      .relativeCell(0, 1).value("Familiar Music Correct Words")
+      .relativeCell(0, 1).value("Familiar Music Incorrect Words")
+      .relativeCell(0, 1).value("Familiar Music Space Characters")
+      .relativeCell(0, 1).value("Familiar Music Space Characters (Unnecessary)")
+      .relativeCell(0, 1).value("Familiar Music Time (Seconds)")
+      .relativeCell(0, 1).value("Familiar Music Character Accuracy")
+      .relativeCell(0, 1).value("Familiar Music Characters per Minute")
+      .relativeCell(0, 1).value("Familiar Music Word Accuracy")
+      .relativeCell(0, 1).value("Familiar Music Words per Minute")
+      .relativeCell(0, 1).value("Familiar Music Real Words per Minute")
+
+      .relativeCell(0, 1).value("Unfamiliar Music Correct Characters")
+      .relativeCell(0, 1).value("Unfamiliar Music Incorrect Characters")
+      .relativeCell(0, 1).value("Unfamiliar Music Correct Words")
+      .relativeCell(0, 1).value("Unfamiliar Music Incorrect Words")
+      .relativeCell(0, 1).value("Unfamiliar Music Space Characters")
+      .relativeCell(0, 1).value("Unfamiliar Music Space Characters (Unnecessary)")
+      .relativeCell(0, 1).value("Unfamiliar Music Time (Seconds)")
+      .relativeCell(0, 1).value("Unfamiliar Music Character Accuracy")
+      .relativeCell(0, 1).value("Unfamiliar Music Characters per Minute")
+      .relativeCell(0, 1).value("Unfamiliar Music Word Accuracy")
+      .relativeCell(0, 1).value("Unfamiliar Music Words per Minute")
+      .relativeCell(0, 1).value("Unfamiliar Music Real Words per Minute")
+
+      .relativeCell(0, 1).value("First Song Feelings")
+      .relativeCell(0, 1).value("Second Song Feelings");
+
+
+      workbook.deleteSheet("Sheet1");
+
+
       return workbook.toFileAsync(path, {password: props.pw});
     });
   });
